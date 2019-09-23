84cdb22882fabea76cc896523a5532f48b8bfe8b
genetyx8 <cedric.simal@gmail.com>
2019-09-23 18:48:48 +0200

    boop

---
+++ mandelbrot.jl
@@ -0,0 +1,32 @@
+#using Plots
+using PlotlyJS
+
+function untildivergence(f::Function, x, maxiter = 30)
+    i = 0
+    while abs(x) < 2 && (i += 1) <= maxiter
+        x = f(x)
+    end
+    return i
+end
+
+f(c) = z -> z^2 + c
+
+const w, h = 1000, 1000
+const zoom = 0.5
+const moveX = 0
+const moveY = 0
+
+img = Array{Int,2}(undef,w,h)
+
+for x in 1:w
+    for y in 1:h
+        c = Complex(
+            (2*x - w) / (w * zoom) + moveX,
+            (2*y - h) / (h * zoom) + moveY
+        )
+        img[y,x] = untildivergence(f(c),0)
+    end
+end
+
+#plotlyjs()
+PlotlyJS.plot(PlotlyJS.heatmap(z=img))
