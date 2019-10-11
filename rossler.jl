50bb3d468e7a948f98c79bf620e688866650f240
genetyx8 <cedric.simal@gmail.com>
2019-10-11 23:59:49 +0200

    fuckin conda

--- rossler.jl
+++ rossler.jl
@@ -1,24 +1,26 @@
 using Plots
-# define the Lorenz attractor
-mutable struct Rossler
-    dt; a; b; c; x; y; z
+using DifferentialEquations
+#using PyPlot
+
+
+p = [0.2,0.2,5.7]
+
+
+function rossler(du,u,p,t)
+    x, y, z = u
+    a, b, c = p
+    du[1] = -y -z
+    du[2] = x + a*y
+    du[3] = b + z*(x-c)
 end
 
-function step!(r::Rossler)
-    dx = -(r.y + r.z)       ; r.x += r.dt * dx
-    dy = r.x + r.a*r.y      ; r.y += r.dt * dy
-    dz = r.b + r.z*(r.x-r.c); r.z += r.dt * dz
-end
+u0 = [5., 5., 1.]
+tspan = (0.0,100.0)
 
-attractor = Rossler((dt = 0.02, a=0.2, b=0.2, c=5.7, x = 5., y = 5., z = 1.)...)
+prob = ODEProblem(rossler, u0, tspan, p)
+sol = solve(prob)
 
-
-# initialize a 3D plot with 1 empty series
-plt = plot3d(1, xlim=(-10,15), ylim=(-30,10), zlim=(0,30),
-                title = "Rössler Attractor", marker = 2)
-
-# build an animated gif by pushing new points to the plot, saving every 10th frame
-@gif for i=1:5000
-    step!(attractor)
-    push!(plt, attractor.x, attractor.y, attractor.z)
-end every 10
+#plotlyjs()
+pyplot()
+pygui(true)
+plot(sol,vars=(1,2,3))
